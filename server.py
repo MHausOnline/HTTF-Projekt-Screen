a6db4e1110d78af8eb6e294886233074bcf92c12
Nils-nonline <nils-nonline@web.de>
2025-11-15 18:59:16 +0100

    should work. untested

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ arrow_events = []
 devices = []
 role = {}
 dimensions = {}
+opposite = {"left":"right","right":"left","top":"bottom","bottom":"top"}
+vectors = {"left":[-1,0],"left":[1,0],"top":[0,-1],"top":[0,1],}
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
@@ -26,7 +28,44 @@ def createRoom(sid):
 def enterRoom(sid,roomName):
     sio.enter_room(sid,roomName)
     sio.emit("joined_room",{"room":roomName},to=sid)
+
+def device_pos_set(sid):
+    for i in range(0,len(devices)):
+        if devices[i]["sid"] == sid:
+            return i
     
+    return None
+
+def arrow_check():
+    for i arrow_events:
+        for y in arrow_events:
+            if i["dir"] == opposite[y["dir"]]:
+                if device_pos_set(i["sid"]) != None and device_pos_set(y["sid"]):
+                    print("the user made a mistake")
+                    return
+
+                if device_pos_set(i["sid"]) != None:
+                    index = device_pos_set(i["dir"])
+
+                    vec = vectors[i["dir"]]
+
+                    x_p = devices[index]["pos"][0] + vec[0]
+                    y_p = devices[index]["pos"][1] + vec[1]
+
+                    devices.append({"sid":y["sid"],"pos":[x_p,y_p]})
+                
+                if device_pos_set(y["sid"]) != None:
+                    index = device_pos_set(y["dir"])
+
+                    vec = vectors[y["dir"]]
+
+                    x_p = devices[index]["pos"][0] + vec[0]
+                    y_p = devices[index]["pos"][1] + vec[1]
+
+                    devices.append({"sid":i["sid"],"pos":[x_p,y_p]})
+
+
+
 
 @sio.event
 def connect(sid, environ, auth):
@@ -51,15 +90,19 @@ def set_role(sid, data):
 def arrow_pressed(sid, data):
     direction = data.get("dir")
 
+    print("arrow pressed in direction",data["dir"])
+
     if direction not in {"right", "left", "top", "bottom"}:
         print(f"problem whith direction {sid}: {direction}")
         return
+
     arrow_events.append({
         "sid": sid,
         "dir": direction,
         "timestamp": time.time()
     })
 
+    arrow_check()
         
 
 @sio.event
@@ -79,14 +122,14 @@ def disconnect(sid):
 def sendAdmin(sid, data):
     for i,value in enumerate(role):
         if value["role"] == "admin":
-            sio.emit("messageAdmin",to=i)
+            sio.emit("messageAdmin", data, to=i)
             break
 
 @sio.event
 def sendAll(sid, data):
     for f, value in enumerate(role):
          if value["role"] == "client":
-            sio.emit("messageAll", to=f)
+            sio.emit("messageAll", data, to=f)
             break
 
 @sio.event
