2ae6918782e8d3faeb58f1d824093b7235feb286
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2025-11-15 16:56:44 +0100

    Add files via upload

--- double_screen_extension/background.js
+++ double_screen_extension/background.js
@@ -1,1 +1,31 @@
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
+const socket = io();
+
+function registerData(request, sender, sendResponse){
+	io.emit(request.type,request.data)
+}
+browser.runtime.onMessage.addListener(sendData)
+
+function sendMessage(event,message){
+	browser.tabs
+	.query({
+		currentWindow: true,
+		active: true,
+	})
+	.then(
+		(tabs)=>{
+			browser.tabs.sendMessage(
+			tabs,
+			message
+			)
+		}
+	)
+}
+
+socket.on("connect", () => {
+	let events = ["messageAll","messageAdmin","set_uuid","joined_room"]
+	for(let ev of events){
+		socket.io.on(ev,(data)=>{sendMessage(ev,data)})
+	}
+});
 
