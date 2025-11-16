605d11f8a66f80063cdef3610dd8194f2d7fc001
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2025-11-16 12:12:10 +0100

    Update background.js

--- double_screen_extension/background.js
+++ double_screen_extension/background.js
@@ -1,35 +1,3 @@
-const socket = io("http://localhost:8000");
-
-socket.emit("join_room",{"room":""})
-
-function sendData(request, sender, sendResponse){
-	socket.emit(request.type,request.data)
-}
-browser.runtime.onMessage.addListener(sendData)
-
-function sendMessage(event,message){
-	browser.tabs
-	.query({
-		currentWindow: true,
-		active: true,
-	})
-	.then(
-		(tabs)=>{
-			browser.tabs.sendMessage(
-			tabs,
-			message
-			)
-		}
-	)
-}
-
-socket.io.on("connect", () => {
-	let events = ["messageAll","messageAdmin","set_uuid","joined_room"]
-	for(let ev of events){
-		socket.on(ev,(data)=>{sendMessage({"type":ev,"data":data})})
-	}
-});
-
 console.log("running...")
 
 
